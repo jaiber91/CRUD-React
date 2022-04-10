a39505ed2b6096a90766b2c7142db0b24580519f
jaiber <jaiver.navia@gmail.com>
2022-04-10 13:41:17 -0500

    maquetado de la vista de registro

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -8,12 +8,12 @@ const Login = () => {
     <form className='form'>
       <h3>Login</h3>
         <div className='form-email'>
-          <label>Email</label>
-          <input className='' type="email" placeholder='email@email.com' />
+          <label for='email'>Email</label>
+          <input type="email" id='email' placeholder='email@email.com' />
         </div>
         <div className='form-password'>
-          <label>Password</label>
-          <input className='' type="password" placeholder='type your password' />
+          <label for='password'>Password</label>
+          <input type="password" id='password' placeholder='type your password' />
         </div>
         <input className='form-btn' type="submit" value='Login' />
         <h5>Create an account</h5>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import './register.css'
+
 
 const Register = () => {
   return (
     <>
-    <h1>Hello Register</h1>
+    
+    <form className='register'>
+      <h3>Create an account</h3>
+        <div className='register-name'>
+          <label for="name">Name</label>
+          <input  type="text" id='name' placeholder='Jhon Doe' />
+        </div>
+        <div className='register-email'>
+          <label for="email">Email</label>
+          <input  type="email" id="email"placeholder='correo@correo.com' />
+        </div>
+        <div className='register-password'>
+          <label for="password" >Password</label>
+          <input type="password" id='password' placeholder='type your password' />
+        </div>
+        <div className='register-RepeatPassword'>
+          <label for='repeat'>Repeat Password</label>
+          <input type="password" id="repeat" placeholder='type your password' />
+        </div>
+        <input className='register-btn' type="submit" value='Create' />
+        
+    </form>
     </>
   )
 }
