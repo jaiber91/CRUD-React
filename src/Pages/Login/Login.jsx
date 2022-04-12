e3148cbfb05694b1617bf8acf33338de9d11a40c
jaiber <jaiver.navia@gmail.com>
2022-04-12 13:45:55 -0500

    validaciones del form login terminadas

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,19 +1,66 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+//import axios from "axios";
 import './Login.css'
-
+import Error from '../../Components/Error/Error'
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState(false);
+
+  const handleSubmitLog = (e) =>{
+    e.preventDefault();
+
+    
+     //Validando que todos los campos no estén vacios
+     if ([email, password].includes("")) {
+      setError(true);
+      console.log("llenar todos los campos");
+    } else {
+      
+      setError(false);
+      console.log("campos ok");
+      setEmail('')
+      setPassword('')
+    }
+
+    
+}
+ 
+
+
+
+
+
+
+
+
   return (
     <>
     
-    <form className='form'>
+    <form className='form' onSubmit={handleSubmitLog}>
+     {error && <Error message="verify email and password" />}
       <h3>Login</h3>
         <div className='form-email'>
-          <label for='email'>Email</label>
-          <input type="email" id='email' placeholder='email@email.com' />
+          <label htmlFor='email'>Email</label>
+          <input 
+          type="email" 
+          id='email'
+          value={email}
+          placeholder='email@email.com' 
+          onChange={(e) => setEmail(e.target.value)}/>
         </div>
+
         <div className='form-password'>
-          <label for='password'>Password</label>
-          <input type="password" id='password' placeholder='type your password' />
+          <label htmlFor='password'>Password</label>
+          <input 
+          type="password" 
+          id='password' 
+          value={password}
+          placeholder='type your password' 
+          onChange={(e) => setPassword(e.target.value)}/>
         </div>
         <input className='form-btn' type="submit" value='Login' />
         
